7e786f7c10f4935b7cba0e29ca641f19ef8c0aa0
bestvist <bestvist@qq.com>
2018-09-03 17:15:52 +0800

    input

--- components/input/input.vue
+++ components/input/input.vue
@@ -1,25 +1,60 @@
 <template>
     <div class="b-input">
         <template v-if="type!=='textarea'">
-            <span></span>
-            <input class="b-input__inner"
+            <span v-if="prefix"
+                ref="prefix"
+                class="b-input__prefix">{{prefix}}</span>
+            <input ref="input"
+                class="b-input__inner"
                 :value="value"
+                :name="name"
+                :minlength="min"
+                :maxlength="max"
                 :disabled="disabled"
                 :readonly="readonly"
+                :step="step"
+                :autofocus="autofocus"
                 :autocomplete="autocomplete"
-                :placeholder="placeholder" />
+                :placeholder="placeholder"
+                @input="handleInput"
+                @focus="handleFocus"
+                @blur="handleBlur"
+                @chang="handleChange"
+                @compositionstart="handleComposition"
+                @compositionupdate="handleComposition"
+                @compositionend="handleComposition" />
+            <span v-if="suffix"
+                ref="suffix"
+                class="b-input__suffix">{{suffix}}</span>
         </template>
-        <textarea class="b-textarea__inner"
-            v-else
+        <textarea v-else
+            class="b-textarea__inner"
+            :style="{'height':height}"
+            :class="className"
             :value="value"
+            :name="name"
+            :minlength="min"
+            :maxlength="max"
             :disabled="disabled"
             :readonly="readonly"
-            :resize="'none'"
-            :placeholder="placeholder"></textarea>
+            :step="step"
+            :autofocus="autofocus"
+            :placeholder="placeholder"
+            @input="handleInput"
+            @focus="handleFocus"
+            @blur="handleBlur"
+            @chang="handleChange"
+            @compositionstart="handleComposition"
+            @compositionupdate="handleComposition"
+            @compositionend="handleComposition"></textarea>
     </div>
 </template>
 
 <script>
+export function isKorean(text) {
+    const reg = /([(\uAC00-\uD7AF)|(\u3130-\u318F)])+/gi;
+    return reg.test(text);
+}
 export default {
     name: "BInput",
     componentName: "BInput",
@@ -29,6 +64,9 @@ export default {
             default: "input"
         },
         value: [String, Number],
+        name: String,
+        min: Number,
+        max: Number,
         placeholder: String,
         disabled: Boolean,
         readonly: Boolean,
@@ -36,6 +74,9 @@ export default {
             type: Boolean,
             default: false
         },
+        step: [String, Number],
+        autofocus: Boolean,
+        height: String, // default: 80px
         autocomplete: {
             type: String,
             default: "off"
@@ -44,6 +85,84 @@ export default {
         suffix: String,
         prefixIcon: String,
         suffixIcon: String
+    },
+    data() {
+        return {
+            currentValue:
+                this.value === void 0 || this.value === null ? "" : this.value,
+            isOnComposition: false,
+            valueBeforeComposition: null
+        };
+    },
+    computed: {
+        className() {
+            return {
+                "is-resize": this.resize
+            };
+        }
+    },
+    methods: {
+        handleInput(e) {
+            const value = e.target.value;
+            this.setCurrentValue(value);
+            if (this.isOnComposition) return;
+            this.$emit("input", value);
+        },
+        handleFocus(e) {
+            this.$emit("focus", e);
+        },
+        handleBlur(e) {
+            this.$emit("blur", e);
+        },
+        handleChange(e) {
+            this.$emit("change", e.target.value);
+        },
+        handleComposition(e) {
+            if (e.type === "compositionend") {
+                this.isOnComposition = false;
+                this.currentValue = this.valueBeforeComposition;
+                this.valueBeforeComposition = null;
+                this.handleInput(e);
+            } else {
+                const value = e.target.value;
+                const lastChar = value[value.length - 1] || "";
+                this.isOnComposition = !isKorean(lastChar);
+                if (this.isOnComposition && e.type === "compositionstart") {
+                    this.valueBeforeComposition = value;
+                }
+            }
+        },
+        setCurrentValue(value) {
+            if (this.isOnComposition && value === this.valueBeforeComposition)
+                return;
+            this.currentValue = value;
+        },
+        computeOffset($input, place) {
+            const rect =
+                this.$refs[place] && this.$refs[place].getBoundingClientRect();
+            let padding = rect ? `${rect.right - rect.left + 10}px` : null;
+
+            if (place === "prefix") {
+                $input.style.paddingLeft = padding;
+            } else {
+                $input.style.paddingRight = padding;
+            }
+        },
+        updateOffset() {
+            const input = this.$refs["input"];
+            if (input) {
+                this.computeOffset(input, "prefix");
+                this.computeOffset(input, "suffix");
+            }
+        }
+    },
+    mounted() {
+        this.updateOffset();
+    },
+    updated() {
+        this.$nextTick(() => {
+            this.updateOffset();
+        });
     }
 };
 </script>
--- examples/views/Input.vue
+++ examples/views/Input.vue
@@ -1,16 +1,28 @@
 <template>
     <div>
         <div class="panel">
-            <b-input></b-input>
+            <b-input v-model="value"></b-input>
         </div>
         <div class="panel">
             <b-input type="textarea"></b-input>
+        </div>
+        <div class="panel">
+            <b-input prefix="$: "></b-input>
+            <b-input suffix="qq.com"></b-input>
         </div>
     </div>
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            value: ""
+        };
+    },
+    mounted() {
+    }
+};
 </script>
 <style lang="scss">
 </style>
