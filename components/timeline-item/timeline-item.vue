9daefc5c805d2a6e15a8e73bce8c70a2386c28a6
bestvist <bestvist@qq.com>
2018-09-20 00:00:22 +0800

    build: timeline

---
+++ components/timeline-item/timeline-item.vue
@@ -0,0 +1,14 @@
+<template>
+    <li class="b-timeline-item">
+        <slot></slot>
+    </li>
+</template>
+
+<script>
+export default {
+    name: "BTimelineItem",
+    componentName: "BTimelineItem",
+    
+};
+</script>
+
---
+++ components/timeline/timeline.vue
@@ -0,0 +1,13 @@
+<template>
+    <ul class="b-timeline">
+        <slot></slot>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: "BTimeline",
+    componentName: "BTimeline"
+};
+</script>
+
