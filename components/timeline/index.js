30d4dae8058b366582a54c9a8c664036a0ff5a4a
bestvist <bestvist@qq.com>
2018-09-20 17:22:18 +0800

    modify: timeline icon

--- components/timeline/index.js
+++ components/timeline/index.js
@@ -1,7 +1,11 @@
 import Timeline from './timeline.vue';
+import TimelineItem from '../timeline-item';
+import Icon from '../icon';
 
 Timeline.install = function (Vue) {
     Vue.component(Timeline.name, Timeline);
+    Vue.component(TimelineItem.name, TimelineItem);
+    Vue.component(Icon.name, Icon);
 }
 
 export default Timeline;
