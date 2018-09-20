c5016d30c1d831e93834cad1a282505059ea6dc0
bestvist <bestvist@qq.com>
2018-09-20 23:45:10 +0800

    modify: carousel animation

--- components/carousel/carousel.vue
+++ components/carousel/carousel.vue
@@ -7,24 +7,24 @@
 <script>
 import BIcon from "../icon";
 export default {
-    name: "BCarousel",
-    componentName: "BCarousel",
-    components: { BIcon },
-    props: {
-        height: String,
-        loop: {
-            type: Boolean,
-            default: true
-        },
-        interval: {
-            type: Number,
-            default: 5000
-        },
-        index: {
-            type: Number,
-            default: 0
-        }
+  name: "BCarousel",
+  componentName: "BCarousel",
+  components: { BIcon },
+  props: {
+    height: String,
+    loop: {
+      type: Boolean,
+      default: true
+    },
+    interval: {
+      type: Number,
+      default: 5000
+    },
+    index: {
+      type: Number,
+      default: 0
     }
+  }
 };
 </script>
 
