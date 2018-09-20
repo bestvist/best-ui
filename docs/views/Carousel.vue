015a4058e3c197b0c05248878dc387119606d880
bestvist <bestvist@qq.com>
2018-09-20 18:47:47 +0800

    modify: carousel item

--- docs/views/Carousel.vue
+++ docs/views/Carousel.vue
@@ -2,16 +2,24 @@
     <div>
         <div class="panel">
             <b-carousel>
-                <b-carousel-item v-for="i in 4" :key="i"></b-carousel-item>
+                <b-carousel-item v-for="i in 4" :key="i">
+                    {{i}}
+                </b-carousel-item>
             </b-carousel>
         </div>
     </div>
 </template>
 
 <style lang="scss">
+.b-carousel-item {
+    text-align: center;
+    line-height: 400px;
+}
+
 .b-carousel-item:nth-of-type(2n-1) {
     background: #eee;
 }
+
 .b-carousel-item:nth-of-type(2n) {
     background: #ccc;
 }
