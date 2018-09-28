4acd6b84dfbc0b69ebcb11e3b624c07e929bb8a9
bestvist <bestvist@qq.com>
2018-09-28 15:53:38 +0800

    add: git corner

--- components/carousel/carousel.vue
+++ components/carousel/carousel.vue
@@ -1,30 +1,36 @@
 <template>
-    <div class="b-carousel">
-        <slot></slot>
-    </div>
+  <div class="b-carousel">
+    <slot></slot>
+    
+  </div>
 </template>
 
 <script>
 import BIcon from "../icon";
 export default {
-  name: "BCarousel",
-  componentName: "BCarousel",
-  components: { BIcon },
-  props: {
-    height: String,
-    loop: {
-      type: Boolean,
-      default: true
-    },
-    interval: {
-      type: Number,
-      default: 5000
-    },
-    index: {
-      type: Number,
-      default: 0
+    name: "BCarousel",
+    componentName: "BCarousel",
+    components: { BIcon },
+    props: {
+        height: String,
+        trigger: {
+            type: String,
+            default: "click"
+        },
+        loop: {
+            type: Boolean,
+            default: true
+        },
+        interval: {
+            type: Number,
+            default: 5000
+        },
+        index: {
+            type: Number,
+            default: 0
+        },
+        autoplay: Boolean
     }
-  }
 };
 </script>
 
